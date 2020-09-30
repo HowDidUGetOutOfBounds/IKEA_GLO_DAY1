dab3e1b9f5bbd84067a04d8f756dd4038e8a98df
HowDidUGetOutOfBounds <serg.karpik@gmail.com>
2020-09-30 21:09:56 +0300

    ended task 1

--- script/index.js
+++ script/index.js
@@ -15,11 +15,17 @@ console.log('varName: ', varName);
 
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overlay = document.querySelector('.overlay');
+//const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const catalogList = document.querySelector('.catalog-list');
 const subCatalog = document.querySelector('.subcatalog');
 const subCatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 
 const openMenu =  () => {
@@ -30,6 +36,7 @@ const openMenu =  () => {
 const closeMenu =  () => {
     catalog.classList.remove('open');
     overlay.classList.remove('active');
+    closeSubMenu();
 };
 
 const openSubMenu = event => {
@@ -40,14 +47,19 @@ const openSubMenu = event => {
     if (itemList){
         subCatalogHeader.innerHTML = itemList.innerHTML;
         subCatalog.classList.add('subopen');
-    }
+    };
 
 };
+
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+}
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
-catalogList.addEventListener('click', openSubMenu)
+catalogList.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
 
 document.addEventListener('keydown', e => {
     if(e.key == 'Escape')
